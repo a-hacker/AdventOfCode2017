c6a69d1c851fceefe70b5ffa422163b8cb245a5f
Austin Hacker <aehacker@us.ibm.com>
2017-12-08 22:24:30 -0500

    Day 7 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,21 @@
 module Main where
 
-import Register
+import Tower
 import Data.List.Split
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 main :: IO ()
 main = do
         input <- readFile "input.txt"
-        print (updateRegisters (map words (lines input)) (Map.fromList [("??highest", 0)]))
+        let progMap = Map.fromList (map inputToProgram (lines input))
+        print (calcWeight progMap (progMap Map.! "wiapj"))
 
-inputToMap :: String -> (String, [String])
-inputToMap x = let spltStr = (splitOn "-> " x) in
-    do
-        if 1 == (length spltStr)
-            then ((head (words (head spltStr))), [])
-            else ((head (words (head spltStr))), (splitOn ", " (last (tail spltStr))))
+inputToProgram :: String -> (String, Program)
+inputToProgram x = do
+    let splitStr = (splitOn "-> " x)
+    let name = head (words (head splitStr))
+    let size = read ((words (head splitStr)) !! 1)
+    if 1 == (length splitStr)
+        then (name, (Program name size []))
+        else (name, (Program name size (splitOn ", " (last (tail splitStr)))))
--- src/tower.hs
+++ src/tower.hs
@@ -1,7 +1,19 @@
 module Tower where
 
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Data.List
+
+data Program = Program String Int [String] deriving (Show)
+
+calcWeight :: Map.Map String Program -> Program -> Int
+calcWeight _ (Program _ weight []) = weight
+calcWeight mp (Program name weight depStrs)
+    -- if all the weights of the children are the same, sum them with this program's weight
+    | (Set.size (Set.fromList (map snd deps))) == 1 = weight + (sum (map snd deps))
+    -- this is hacky but if the childrens sums don't add up, just error showing deps so I can compute by hand the value it should be
+    | otherwise                                      = error (show deps)
+        where deps = zip (map ((Map.!) mp) depStrs) (map (calcWeight mp) (map ((Map.!) mp) depStrs))
 
 topoSort :: Map.Map String [String] -> [[String]] -> [[String]]
 topoSort depMap done
