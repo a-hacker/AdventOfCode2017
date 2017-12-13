444e65b1b366460974ccc18fe4ae7e0e1a6f344a
Austin Hacker <aehacker@us.ibm.com>
2017-12-13 13:32:31 -0500

    Day 13 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ main :: IO ()
 main = do
     input <- readFile "input.txt"
     let scanners = Map.fromList (map inputToTuple (map words (lines input)))
-    print $ navigateFirewall scanners 0 0
+    print $ attemptRun scanners 0
 
 inputToTuple :: [String] -> (Int, Int)
 inputToTuple (key:val:[]) = ((read (init key)), (read val))
--- src/Scanner.hs
+++ src/Scanner.hs
@@ -2,12 +2,17 @@ module Scanner where
 
 import qualified Data.Map as Map
 
-navigateFirewall :: Map.Map Int Int -> Int -> Int -> Int
-navigateFirewall scanners pos total
-    | pos > (fst (Map.findMax scanners)) = total
-    | Map.notMember pos scanners  = navigateFirewall scanners (pos + 1) total
+navigateFirewall :: Map.Map Int Int -> Int -> Int -> Bool
+navigateFirewall scanners pos sec
+    | pos > (fst (Map.findMax scanners)) = True
+    | Map.notMember pos scanners  = navigateFirewall scanners (pos + 1) (sec + 1)
     | otherwise                   = do
-        let caught = pos `mod` (((scanners Map.! pos) - 1) * 2) == 0
+        let caught = sec `mod` (((scanners Map.! pos) - 1) * 2) == 0
         if caught
-            then navigateFirewall scanners (pos + 1) (total + (scanners Map.! pos) * pos)
-            else navigateFirewall scanners (pos + 1) total
+            then False
+            else navigateFirewall scanners (pos + 1) (sec + 1)
+
+attemptRun :: Map.Map Int Int -> Int -> Int
+attemptRun scanners delay
+    | navigateFirewall scanners 0 delay = delay
+    | otherwise                         = attemptRun scanners (delay + 1)
