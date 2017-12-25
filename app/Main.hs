9295d8a58ec466b7dc176902d14b0a70becf936d
Austin Hacker <aehacker@us.ibm.com>
2017-12-24 21:53:50 -0500

    Day 23 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified Data.Set as Set
 import Data.List.Split
+import Data.Numbers.Primes
 
 import Bridge
 
@@ -9,8 +10,8 @@ import Bridge
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    let components = Set.fromList (map inputToComponent (lines input))
-    print $ build components (0, 0) 0
+    let numbers = [109300, 109317..126300]
+    print $ (length numbers) - length (filter isPrime numbers)
 
 inputToComponent :: String -> Component
 inputToComponent rawInput = (Component (read p1) (read p2))
